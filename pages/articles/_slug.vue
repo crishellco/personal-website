658f7ff12a6819c769b7689262a4089618e9a6cb
Chris Mitchell <cmitchell@crishell.co>
2020-10-19 17:07:01 -0600

    M nuxt.config.js  M pages/articles/_slug.vue

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -27,8 +27,6 @@ export default {
     return {
       title: `${article.title} | Chris Mitchell`,
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
           hid: 'description',
           name: 'description',
