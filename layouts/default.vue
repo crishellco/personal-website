68c1870fefbb4e4a884a1228f78b9400250d794e
Christopher Mitchell <cmitchell@crishell.co>
2023-03-31 02:51:55 -0400

    docs: add packages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-10">
     <div class="flex items-center pb-10">
-      <div class="rounded-full overflow-hidden h-16 w-16">
+      <div class="rounded-full overflow-hidden w-20 h-20">
         <img src="~/assets/avatar.jpeg" alt="" />
       </div>
       <div class="ml-4 flex flex-col justify-between">
@@ -10,13 +10,30 @@
           Director of Engineering, Architect @
           <a class="" href="https://www.cruxdata.com/" target="_blank">Crux</a>
         </div>
-        <div class="text-xs">
+        <div class="text-xs space-x-2">
+          <span class="font-semibold">Packages</span>
+          <span class="font-semibold">/</span>
+          <a target="_blank" href="https://crishellco.github.io/vue-call-store/"
+            >@crishellco/vue-call-store</a
+          >
+          <a target="_blank" href="https://crishellco.github.io/vue-beam/"
+            >@crishellco/vue-beam</a
+          >
+          <a target="_blank" href="https://crishellco.github.io/vue-spruce/"
+            >@crishellco/vue-spruce</a
+          >
+          <a target="_blank" href="https://vue-hubble.crishell.co/"
+            >@crishellco/vue-hubble</a
+          >
+          <a target="_blank" href="https://crishellco.github.io/vue-coy/"
+            >@crishellco/vue-coy</a
+          >
+        </div>
+        <div class="text-xs space-x-2">
+          <span class="font-semibold">Links</span>
+          <span class="font-semibold">/</span>
           <a target="_blank" href="http://github.com/crishellco">GitHub</a>
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/crishellco/"
-            class="mx-2
-          "
+          <a target="_blank" href="https://www.linkedin.com/in/crishellco/"
             >LinkedIn</a
           >
         </div>
